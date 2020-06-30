e545f1dbd2d0b5448d54a0ca1fe2b544dc64c781
eyabc <bey4314@naver.com>
2020-06-30 22:33:51 +0900

    #1 : NextJS 소개 및 개발 환경 구성 7 - next/link <Link>와 <a> 차이점

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,16 @@
+import Link from 'next/link';
+
 export default () => (
-  <div>Welcome to next.js!</div>
+  <div>
+    Welcome to next.js!
+    <div>
+      <Link href="/playground">
+        <a>Go playground</a>
+      </Link>
+    </div>
+
+    <div>
+        <a href="/playground">Go playground (without Link)</a>
+    </div>
+  </div>
 )
