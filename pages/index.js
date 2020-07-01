d8a54666f88fd5e201017977f3e3d6e3ec81c8ef
eyabc <bey4314@naver.com>
2020-07-02 00:49:06 +0900

    #3-1 : State와 Mobx - 실행되지 않는 코드

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,39 @@
 import React, { Component } from 'react';
 import faker from 'faker';
+import { decorate, observable } from 'mobx';
+import { observer } from 'mobx-react';
 
-class Index extends Component {
-  state = {
-    name: faker.name.findName(),
-    avatar: faker.image.avatar(),
-    email: faker.internet.email(),
+// observable 관찰 대상
+class Data {
+  avatar = faker.image.avatar();
+  email = faker.internet.email();
+  name = {
+    firstName: faker.name.firstName(),
+    lastName: faker.name.lastName(),
   };
+}
+
+// mobx선언, 변수들을 observable으로 사용하겠다
+decorate(Data, {
+  avatar: observable,
+  email: observable,
+  name: observable,
+});
+
+// observer 관찰자
+class Index extends Component {
+  data = new Data();
 
   generate = () => {
-    this.setState({
-      name: faker.name.findName(),
-      avatar: faker.image.avatar(),
-      email: faker.internet.email(),
-    });
+    console.log('gen');
+    const _this = this.data;
+    _this.email = faker.internet.email();
+    _this.name.firstName = faker.name.firstName();
+    _this.avatar = faker.image.avatar();
   };
 
   render () {
-    const { name, avatar, email } = this.state;
+    const { name, avatar, email } = this.data;
     return (
       <div>
         <h1>Faker Demo</h1>
@@ -25,18 +41,20 @@ class Index extends Component {
         <dl className="row">
           <dt className="col-sm-3">Avatar</dt>
           <dd className="col-sm-9">
-            <img src={ avatar } alt=""/>
+            <img src={ avatar } alt="avatar"/>
           </dd>
           <dt className="col-sm-3">Name</dt>
-          <dd className="col-sm-9">{ name }</dd>
+          <dd className="col-sm-9">{ name.firstName } { name.lastName }</dd>
           <dt className="col-sm-3">Email</dt>
           <dd className="col-sm-9">{ email }</dd>
         </dl>
 
-        <button onClick={ this.generate }>Generate</button>
+        <button className="btn btn-primary" onClick={ this.generate }>Generate</button>
       </div>
     );
   }
 }
+
+Index = observer(Index);
 
 export default Index;
